3218da63ea482cd9d9c8f6ec77ede755c180a6bc
jyb8509 <bestdung8509@gmail.com>
2021-08-20 18:25:29 +0900

    반응형 first

--- js/main.js
+++ js/main.js
@@ -15,6 +15,18 @@ searchInputEl.addEventListener('blur', function () {
     searchInputEl.setAttribute('placeholder', '');
 });
 
+
+
+const toggleBtn = document.querySelector('header .icon-group .toogle_btn');
+const menu = document.querySelector('header .main-menu-mobile');
+const icons = document.querySelector('header .icon-group .material-icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+}); 
+
+
 // ._throttle(함수, 시간(ms)) : 스크롤 위치 계산 : lodash 라이브러리 사용
 // gsap : 천천히 사라지기 위해서 gsap animation 관련 library 사용
 // gsap.to(요소, 지속시간, 옵션)
@@ -36,7 +48,7 @@ window.addEventListener('scroll', _.throttle(function(){
 
 // ABOUT 슬라이드
 
-const slide = new Swiper('.swiper-container', {
+new Swiper('.about .swiper-container', {
     slidesPerView : 2, // 한 슬라이드에 보여줄 갯수
     spaceBetween : 10, // 슬라이드 사이 여백
     loop : true, // 슬라이드 반복 여부
@@ -54,6 +66,28 @@ const slide = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     }
   })
+  
+
+  
+new Swiper('.mobile .swiper-container', {
+    slidesPerView : 1, // 한 슬라이드에 보여줄 갯수
+    spaceBetween : 100, // 슬라이드 사이 여백
+    loop : true, // 슬라이드 반복 여부
+    centeredSlides: true,
+    autoplay : {  // 자동 슬라이드 설정 , 비 활성화 시 false
+      delay : 4000,   // 시간 설정
+      disableOnInteraction : false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+    },
+    pagination : { // 페이징
+        el : '.swiper-pagination',
+        clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+    },
+    navigation: {   // 버튼 사용자 지정
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+  })
+
 
 //  최상단 스크롤 버튼
 
